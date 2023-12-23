d682197f4c14212aa8aa6204cc0dd4ac38f80738
Pascal Böhler <pascal@bembel.dev>
2023-12-23 10:30:31 +0100

    feat: Added Instance class and Authentication file

---
+++ Sources/Swiftodon/Authentication.swift
@@ -0,0 +1,12 @@
+//
+//  Authentication.swift
+//  
+//
+//  Created by Pingu on 23.12.23.
+//
+
+import Foundation
+
+func authenticateDevice(intanceURL: String) -> Instance? {
+    return nil
+}
---
+++ Sources/Swiftodon/Models/Instance.swift
@@ -0,0 +1,25 @@
+//
+//  Instance.swift
+//
+//
+//  Created by Pingu on 23.12.23.
+//
+
+import Foundation
+
+class Instance {
+    var instanceURL: String
+    var isAuthenticated: Bool
+    
+    init(instanceURL: String, isAuthenticated: Bool) {
+        self.instanceURL = instanceURL
+        self.isAuthenticated = isAuthenticated
+    }
+    
+    /// Authenticate device against server to gain access and login user
+    func authenticateDevice() -> Bool {
+        return false
+    }
+    
+    
+}
