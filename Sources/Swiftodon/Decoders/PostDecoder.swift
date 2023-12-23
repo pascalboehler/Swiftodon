ab99e716d7aad21de7c4abd81314a76e93b2545f
Pascal Böhler <pascal@bembel.dev>
2023-12-23 13:03:34 +0100

    feat: Moved decoder mathods to own folder + added PostDecoder file

---
+++ Sources/Swiftodon/Decoders/PostDecoder.swift
@@ -0,0 +1,146 @@
+extension Post {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        /// web
+        case postURL = "url"
+        case postURI = "uri"
+        /// metadata
+        case visibility = "visibility"
+        case repliesCount = "replies_count"
+        case reblogsCount = "reblogs_count"
+        case favouritesCount = "favourites_count"
+        case createdAt = "created_at"
+        case editedAt = "edited_at"
+        case application = "application"
+        case account = "account"
+        case metions = "mentions"
+        /// contents
+        case content = "content"
+        case isReplog = "reblog"
+        case mediaAttachmentsURLs = "media_attachments"
+        case tags = "tags"
+        case emojies = "emojies"
+        case card = "card"
+        case poll = "poll"
+    }
+    
+    public init(from: Decoder) throws {
+        let values = try from.container(keyedBy: CodingKeys.self)
+        let rawID = try values.decode(UInt.self, forKey: .id)
+        let rawPostURL = try values.decode(String.self, forKey: .postURL)
+        let rawPostURI = try values.decode(String.self, forKey: .postURI)
+        let rawVisibility = try values.decode(String.self, forKey: .visibility)
+        let rawRepliesCount = try values.decode(Int.self, forKey: .repliesCount)
+        let rawReblogsCount = try values.decode(Int.self, forKey: .reblogsCount)
+        let rawFavouritesCount = try values.decode(Int.self, forKey: .favouritesCount)
+        let rawCreatedAt = try values.decode(String.self, forKey: .createdAt)
+        let rawEditedAt = try values.decode(String.self, forKey: .editedAt)
+        let rawApplication = try values.decode(Application.self, forKey: .application)
+        let rawAccount = try values.decode(Account.self, forKey: .account)
+        let rawMentions = try values.decode([String].self, forKey: .mentions)
+        let rawContent = try values.decode(String.self, forKey: .content)
+        let rawIsReblog = try values.decode(Bool.self, forKey: .isReplog)
+        let rawMediaAttachmentsURLs = try values.decode([String].self, forKey: .mediaAttachmentsURLs)
+        let rawTags = try values.decode([Any].self, forKey: .tags)
+        let rawEmojies = try values.decode([Any].self, forKey: .emojies)
+        let rawCard = try values.decode(String.self, forKey: .card)
+        let rawPoll = try values.decode(String.self, forKey: .poll)
+
+        guard let id = rawID else {
+            throw DecodingError.dataCorruptedError(forKey: .id, in: values, debugDescription: "ID is not a valid UInt")
+        }
+        
+        guard let postURL = URL(string: rawPostURL) else {
+            throw DecodingError.dataCorruptedError(forKey: .postURL, in: values, debugDescription: "postURL is not a valid URL")
+        }
+
+        guard let postURI = URL(string: rawPostURI) else {
+            throw DecodingError.dataCorruptedError(forKey: .postURI, in: values, debugDescription: "postURI is not a valid URL")
+        }
+
+        guard let visibility = Visibility(rawValue: rawVisibility) else {
+            throw DecodingError.dataCorruptedError(forKey: .visibility, in: values, debugDescription: "visibility is not a valid Visibility")
+        }
+
+        guard let repliesCount = rawRepliesCount else {
+            throw DecodingError.dataCorruptedError(forKey: .repliesCount, in: values, debugDescription: "repliesCount is not a valid Int")
+        }
+
+        guard let reblogsCount = rawReblogsCount else {
+            throw DecodingError.dataCorruptedError(forKey: .reblogsCount, in: values, debugDescription: "reblogsCount is not a valid Int")
+        }
+
+        guard let favouritesCount = rawFavouritesCount else {
+            throw DecodingError.dataCorruptedError(forKey: .favouritesCount, in: values, debugDescription: "favouritesCount is not a valid Int")
+        }
+
+        guard let createdAt = rawCreatedAt else {
+            throw DecodingError.dataCorruptedError(forKey: .createdAt, in: values, debugDescription: "createdAt is not a valid String")
+        }
+
+        guard let editedAt = rawEditedAt else {
+            throw DecodingError.dataCorruptedError(forKey: .editedAt, in: values, debugDescription: "editedAt is not a valid String")
+        }
+
+        guard let application = rawApplication else {
+            throw DecodingError.dataCorruptedError(forKey: .application, in: values, debugDescription: "application is not a valid Application")
+        }
+
+        guard let account = rawAccount else {
+            throw DecodingError.dataCorruptedError(forKey: .account, in: values, debugDescription: "account is not a valid Account")
+        }
+
+        guard let mentions = rawMentions else {
+            throw DecodingError.dataCorruptedError(forKey: .mentions, in: values, debugDescription: "mentions is not a valid [String]")
+        }
+
+        guard let content = rawContent else {
+            throw DecodingError.dataCorruptedError(forKey: .content, in: values, debugDescription: "content is not a valid String")
+        }
+
+        guard let isReblog = rawIsReblog else {
+            throw DecodingError.dataCorruptedError(forKey: .isReplog, in: values, debugDescription: "isReblog is not a valid Bool")
+        }
+
+        guard let mediaAttachmentsURLs = rawMediaAttachmentsURLs else {
+            throw DecodingError.dataCorruptedError(forKey: .mediaAttachmentsURLs, in: values, debugDescription: "mediaAttachmentsURLs is not a valid [String]")
+        }
+
+        guard let tags = rawTags else {
+            throw DecodingError.dataCorruptedError(forKey: .tags, in: values, debugDescription: "tags is not a valid [Any]")
+        }
+
+        guard let emojies = rawEmojies else {
+            throw DecodingError.dataCorruptedError(forKey: .emojies, in: values, debugDescription: "emojies is not a valid [Any]")
+        }
+
+        guard let card = rawCard else {
+            throw DecodingError.dataCorruptedError(forKey: .card, in: values, debugDescription: "card is not a valid String")
+        }
+
+        guard let poll = rawPoll else {
+            throw DecodingError.dataCorruptedError(forKey: .poll, in: values, debugDescription: "poll is not a valid String")
+        }
+
+        self.id = id
+        self.postURL = postURL
+        self.postURI = postURI
+        self.visibility = visibility
+        self.repliesCount = repliesCount
+        self.reblogsCount = reblogsCount
+        self.favouritesCount = favouritesCount
+        self.createdAt = createdAt
+        self.editedAt = editedAt
+        self.application = application
+        self.account = account
+        self.mentions = mentions
+        self.content = content
+        self.isReblog = isReblog
+        self.mediaAttachmentsURLs = mediaAttachmentsURLs
+        self.tags = tags
+        self.emojies = emojies
+        self.card = card
+        self.poll = poll
+    }
+}
--- Sources/Swiftodon/Models/Post.swift
+++ Sources/Swiftodon/Models/Post.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct Post: Identifiable {
     /// Unique post ID supplied by Mastodon
-    public let id: UInt
+    public let id: UInt?
     
     /// Post URLs
     public var postURL: String
@@ -36,38 +36,6 @@ public struct Post: Identifiable {
     public var poll: String?
 }
 
-extension Post {
-    
-    private enum CodingKeys: String, CodingKey {
-        case id = "id"
-        /// web
-        case postURL = "url"
-        case postURI = "uri"
-        /// metadata
-        case visibility = "visibility"
-        case repliesCount = "replies_count"
-        case reblogsCount = "reblogs_count"
-        case favouritesCount = "favourites_count"
-        case createdAt = "created_at"
-        case editedAt = "edited_at"
-        case application = "application"
-        case account = "account"
-        case metions = "mentions"
-        /// contents
-        case content = "content"
-        case isReplog = "reblog"
-        case mediaAttachmentsURLs = "media_attachments"
-        case tags = "tags"
-        case emojies = "emojies"
-        case card = "card"
-        case poll = "poll"
-    }
-    
-    /*sspublic init(from: Decoder) throws {
-        
-    }*/
-}
-
 public struct Application {
     /// Name of the instance
     public var name: String
@@ -76,13 +44,13 @@ public struct Application {
     public var website: String
 }
 
-public enum Visibility {
+public enum Visibility: String, Codable {
     /// Post is public to all instances
-    case `public`
+    case `public` = "public"
     
     /// Post is public for the instance the user is on
-    case community
+    case community = "community"
     
     /// Post is only visible to own followers
-    case `private`
+    case `private` = "private"
 }
