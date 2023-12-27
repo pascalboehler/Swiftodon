cfdb2d4890c51cc3e3d9d83b1e030933def4eba1
Pascal Böhler <pascal@bembel.dev>
2023-12-27 14:10:44 +0100

    feat: Implemented tag decoder

--- Sources/Swiftodon/Decoders/TagDecoder.swift
+++ Sources/Swiftodon/Decoders/TagDecoder.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Tag: Decodable {
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: TagJSONKeys.self)
+        
+        let rawName = try? values.decode(String.self, forKey: .name)
+        let rawURL = try? values.decode(String.self, forKey: .url)
+        
+        guard let name = rawName else {
+            throw DecodingError.dataCorruptedError(forKey: .name, in: values, debugDescription: "Not found")
+        }
+        self.name = name
+        guard let url = rawURL else {
+            throw DecodingError.dataCorruptedError(forKey: .url, in: values, debugDescription: "Not found")
+        }
+        self.url = url
+    }
+    
+    
+    enum TagJSONKeys: String, CodingKey {
+        case name = "name"
+        case url = "url"
+    }
+    
+}
