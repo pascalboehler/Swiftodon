4b1e04ba292cf529506193b7cf2a6424835ab3a4
Pascal Böhler <pascal@bembel.dev>
2023-12-27 14:15:36 +0100

    feat: Added correct classifications to enum

--- Sources/Swiftodon/Models/Visibility.swift
+++ Sources/Swiftodon/Models/Visibility.swift
@@ -1,10 +1,13 @@
 public enum Visibility: String, Codable {
-    /// Post is public to all instances
+    /// Post is public to all instances and shown in public timeline
     case `public` = "public"
     
-    /// Post is public for the instance the user is on
-    case community = "community"
+    /// Post is public, but not included in public timelines
+    case unlisted = "unlisted"
     
-    /// Post is only visible to own followers
+    /// Post is only visible to own followers and any mentioned users
     case `private` = "private"
+    
+    /// Visible only to mentioned users
+    case direct = "direct"
 }
