ae278d036bcc8ed630bd3a0c8e2396afb297d81c
Pascal Böhler <pascal@bembel.dev>
2023-12-28 21:35:15 +0100

    feat: Improved Post struct to mastodon doc

--- Sources/Swiftodon/Models/Post.swift
+++ Sources/Swiftodon/Models/Post.swift
@@ -12,7 +12,7 @@ public struct Post: Identifiable {
     public let id: String
     
     /// Post URLs
-    public var postURL: String?
+    public var postURL: String? /// link to status representation
     public var postURI: String
     
     /// post metadata
@@ -24,27 +24,38 @@ public struct Post: Identifiable {
     public var reblogsCount: Int
     public var favouritesCount: Int
     
+    /// timestamps
     public var createdAt: String
     public var editedAt: String?
+    
+    /// metadata about creator
     public var application: Application?
     public var account: Account
-    public var mentions: [Mention]
     public var language: String?
     
     /// Content of the post including attachments
-    public var content: String
+    public var content: String /// html
+    public var text: String
     public var spoilerText: String
     public var reblog: String?
-    public var mediaAttachmentsURLs: [String]
+    public var mediaAttachments: [MediaAttachment]
         
     /// Replies
     public var inReplyToStatusID: String?
     public var inReplyToAccountID: String?
     
+    /// User status
+    public var favourited: Bool?
+    public var reblogged: Bool?
+    public var muted: Bool?
+    public var bookmarked: Bool?
+    public var pinned: Bool?
+    public var filtered: Bool?
     
     /// Additional data
     public var tags: [Tag]
     public var emojis: [Emoji]
-    public var card: String?
+    public var mentions: [Mention]
+    public var card: Card?
     public var poll: Poll?
 }
