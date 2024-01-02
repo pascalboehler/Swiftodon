46799e52bda8a9b49ce631364e6ba225f92ece17
Pascal Böhler <pascal@bembel.dev>
2024-01-02 14:31:38 +0100

    feat(auth): added code to retrieve auth token

--- Sources/Swiftodon/Models/Instance.swift
+++ Sources/Swiftodon/Models/Instance.swift
@@ -6,10 +6,14 @@
 //
 
 import Foundation
+import Alamofire
 
 public class Instance {
     public var instanceURL: String
     public var isAuthenticated: Bool
+    
+    private var clientSecret: String = ""
+    private var clientId: String = ""
     
     public init(instanceURL: String, isAuthenticated: Bool) {
         self.instanceURL = instanceURL
@@ -17,7 +21,16 @@ public class Instance {
     }
     
     /// Authenticate device against server to gain access and login user
-    public func authenticateDevice() -> Bool {
+    @available(iOS 13.0.0, *)
+    @available(macOS 10.15, *)
+    public func authenticateApplication(for appName: String = "Swiftodon-Package-Test", website: String = "https://test.example") async throws -> Bool {
+        
+        let params = ["client_name": "\(appName)", "redirect_uris": "urn:ietf:wg:oauth:2.0:oob", "scopes": "read write push", "website": "\(website)"]
+        
+        let response = try await AF.request("https://\(instanceURL)/api/v1/apps", method: .post, parameters: params, encoder: URLEncodedFormParameterEncoder.default).serializingString().value
+        
+        print(response)
+        
         return false
     }
 }
